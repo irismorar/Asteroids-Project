6a19f54688e9b6390210bba2e638a36eb38b0579
Iris Adina Morar <morariris@gmail.com>
2024-09-27 14:10:45 +0300

    create a new file called constants.py containing some given variables. Import it to the main.py file and also import pygame.Initialize all imported pygame modules using pygame.init() at the beginning of the main() function.

--- main.py
+++ main.py
@@ -2,11 +2,17 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+from constants import *
 
 def main():
+  pygame.init()
   print("Starting asteroids!")
+  print(f"Screen width: {SCREEN_WIDTH}")
+  print(f"Screen height: {SCREEN_HEIGHT}")
 
 if __name__ == "__main__":
     main()
 
-#This line ensures the main() function is only called when this file is run directly; it won't run if it's imported as a module. It's considered the "pythonic" way to structure an executable program in Python. Technically, the program will work fine by just calling main(), but you might get an angry letter from Guido van Rossum if you don't.
+#This line ensures the main() function is only called when this file is run directly; it won't run if it's imported as a module.
+#It's considered the "pythonic" way to structure an executable program in Python. Technically,
+#the program will work fine by just calling main(), but you might get an angry letter from Guido van Rossum if you don't.

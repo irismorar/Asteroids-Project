5ae6749f2114562b3822eeed50b802b0cfa3ce81
Iris Adina Morar <morariris@gmail.com>
2024-09-30 13:29:19 +0300

    restrict the game to draw a maximum of 60 times per second, or 60 FPS (frames per second) by using pygame.time.Clock object and the method .tick()

--- main.py
+++ main.py
@@ -1,25 +1,36 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
+# this allows us to use code from the open-source pygame library throughout this file
 import pygame
 from constants import *
 
 def main():
+  # initialising the pygame
   pygame.init()
+  # creating display
   screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
   
-  while True:
+  # creating a clock object
+  clock = pygame.time.Clock()
+  dt = 0
+  
+  # creating a running loop
+  while True: 
+     # creating a loop to check events that are occurring and make the X button of the frame functionable
      for event in pygame.event.get():
         if event.type == pygame.QUIT:
           return
      
+     # setting a background
      screen.fill((0,0,0))
+     # updating the display
      pygame.display.flip()
+
+     # pausing the game loop until 1/60th of a second has passed
+     # the .tick() method also returns the amount of time that has passed since the last time it was called: the delta time (dt variable)
+     dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
     main()
-
 #This line ensures the main() function is only called when this file is run directly; it won't run if it's imported as a module.
 #It's considered the "pythonic" way to structure an executable program in Python. Technically,
 #the program will work fine by just calling main(), but you might get an angry letter from Guido van Rossum if you don't.

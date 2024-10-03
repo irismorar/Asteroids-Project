2dc9965f59c84d61fe089fab737e04441b8078f9
Iris Adina Morar <morariris@gmail.com>
2024-10-03 18:30:46 +0300

    destroy asteroids by shooting them. In main.py file this means that if a bullet and an asteroid collide, call the .kill() method on both objects to remove them from the game - the .kill() method is a feature built-in to pygame; it will remove the object from all of its groups, so the game will stop drawing and updating it automatically.

--- main.py
+++ main.py
@@ -54,11 +54,18 @@ def main():
       obj.draw(screen)
     
     #12.
-    pygame.display.flip()
+    for asteroid in asteroids:
+      for shot in shots:
+        if shot.collides(asteroid):
+          shot.kill()
+          asteroid.kill()
+    
     #13.
+    pygame.display.flip()
+    #14.
     dt = clock.tick(60) / 1000
   
-#14.
+#15.
 if __name__ == "__main__":
   main()
 
@@ -75,6 +82,7 @@ if __name__ == "__main__":
 #6.1 this ensures that every instance of the Asteroid class is automatically added to these groups upon creation
 #6.2 set the static containers field of the AsteroidField class to only the updatable group (it's not drawable, and it's not an 
      # asteroid itself)
+#6.3 set the static containers field of the Shot class to shots, updatable and drawable groups
 #7. creating the player object
 #7.1 create a new AsteroidField object in the initialization code
 #8. creating a running loop
@@ -82,10 +90,13 @@ if __name__ == "__main__":
 #10. setting a background
 #11. iterate over all "updatables" and .update() them, then iterate over all "drawables" and .draw() them, then iterate over asteroids and 
      # detect collisions using .collides()
-#12. updating the display
-#13. pausing the game loop until 1/60th of a second has passed
+#12. If a bullet and an asteroid collide, call the .kill() method on both objects to remove them from the game.
+     # The kill() method is a feature built-in to pygame; it will remove the object from all of its groups, 
+     # so our game will stop drawing and updating it automatically.
+#13. updating the display
+#14. pausing the game loop until 1/60th of a second has passed
      # the .tick() method also returns the amount of time that has passed since the last time it was called: the delta time (dt variable)
-#14. This line ensures the main() function is only called when this file is run directly; it won't run if it's imported as a module.
+#15. This line ensures the main() function is only called when this file is run directly; it won't run if it's imported as a module.
      # It's considered the "pythonic" way to structure an executable program in Python. Technically,the program will work fine 
      # by just calling main(), but you might get an angry letter from Guido van Rossum if you don't.
 
